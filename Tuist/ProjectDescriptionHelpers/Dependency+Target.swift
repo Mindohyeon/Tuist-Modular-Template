b757ac5b5dc0900fbd2a06d00ce995ea90ceb779
Mindohyeon <min50875@naver.com>
2023-09-01 12:26:52 +0900

    Add Dependency+SPM

--- Tuist/ProjectDescriptionHelpers/Dependency+Target.swift
+++ Tuist/ProjectDescriptionHelpers/Dependency+Target.swift
@@ -1,16 +1,12 @@
 import ProjectDescription
 
 extension TargetDependency{
-    public struct SPM {}
     struct Core {}
     struct Domain {}
     struct Shared {}
 }
 
-public extension TargetDependency.SPM {
-    static let SnapKit = TargetDependency.external(name: "SnapKit")
-    static let Then = TargetDependency.external(name: "Then")
-}
 
-public extension TargetDependency.Shared {
+extension TargetDependency.Shared {
+    static let GlobalThirdPartyLib = TargetDependency.shared(name: "GlobalThirdPartyLib")
 }
